5da7384c45ebdd80fc191fb26245d01a597c8672
consolexin <consolexin@gmail.com>
2022-08-16 14:32:35 +0800

    daytime

---
+++ 05/daytime.cpp
@@ -0,0 +1,41 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <netdb.h>
+
+
+int main(int argc, char** argv) {
+    assert(argc == 2);
+    char *host = argv[1];
+    struct hostent* hostinfo = gethostbyname(host);
+    assert(hostinfo);
+
+    struct servent* servinfo = getservbyname("daytime", "tcp");
+    assert(servinfo);
+
+    printf("daytime port is %d\n", ntohs(servinfo->s_port));
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = servinfo->s_port;
+    addr.sin_addr = *(struct in_addr*)*hostinfo->h_addr_list;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int result = connect(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+    assert(result != -1);
+
+    char buffer[128];
+    result = read(sockfd, buffer, sizeof(buffer));
+    assert(result > 0);
+    buffer[result] = 0;
+    printf("the daytime  is :%s", buffer);
+    close(sockfd);
+    return 0;
+}
