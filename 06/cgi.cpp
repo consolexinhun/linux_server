b19fdbd4118b9a977b224e3841c7d59e5f5a84e6
consolexin <consolexin@gmail.com>
2022-08-16 16:05:59 +0800

    cgi

---
+++ 06/cgi.cpp
@@ -0,0 +1,51 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char** argv) {
+    if (argc <= 2) {
+        printf("usage: %s ip port\n", basename(argv[0]));
+        return 1;
+    }
+
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
+
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    int ret = bind(sock, (struct sockaddr*)&addr, sizeof(addr));
+    assert(ret != -1);
+
+    ret = listen(sock, 5);
+    assert(ret != -1);
+
+    struct sockaddr_in client;
+    socklen_t client_size = sizeof(client);
+    int connfd = accept(sock, (struct sockaddr*)&client, &client_size);
+    if (connfd < 0) {
+        printf("errno : %d\n", errno);
+    } else {
+        close(STDOUT_FILENO);
+        dup(connfd);
+        printf("abcd\n");
+        close(connfd);
+    }
+    close(sock);
+    return 0;
+}
